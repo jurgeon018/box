9e6af8b092a2eb85b334cea1c019e76efcbca32c
andrew mendela <jurgeon018@gmail.com>
2020-08-03 13:54:06 +0300

    s

--- apps/sw_shop/sw_order/views.py
+++ apps/sw_shop/sw_order/views.py
@@ -8,10 +8,10 @@ from box.apps.sw_payment.liqpay.utils import create_liqpay_transaction
 
 
 def liqpay_callback(request):
-  form          = create_liqpay_transaction(request)
-  transaction   = form.instance
-  order = Order.objects.get(id=transaction.order_id)
-  payment       = Payment.objects.create(
+  form        = create_liqpay_transaction(request)
+  transaction = form.instance
+  order       = Order.objects.get(id=transaction.order_id)
+  payment     = Payment.objects.create(
     order=order,
     amount=transaction.amount,
     currency=Currency.objects.get(code=transaction.currency)
